77660d6aae960c89b321407f9cb97ec9846475cc
Fu Hanxi <fuhanxi@espressif.com>
2025-01-14 12:54:13 +0100

    docs: init documentation with api references

---
+++ docs/conf_common.py
@@ -0,0 +1,65 @@
+# SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import subprocess
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'idf-ci'
+project_homepage = 'https://github.com/espressif/idf-ci'
+copyright = '2025, Espressif Systems (Shanghai) Co., Ltd.'  # noqa: A001
+author = 'Fu Hanxi'
+languages = ['en']
+version = '0.x'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_copybutton',
+    'myst_parser',
+    'sphinxcontrib.mermaid',
+    'sphinxarg.ext',
+    'sphinx_tabs.tabs',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_css_files = ['theme_overrides.css']
+html_logo = '../_static/espressif-logo.svg'
+html_static_path = ['../_static']
+html_theme = 'sphinx_rtd_theme'
+#
+# # mermaid 10.2.0 will show syntax error
+# # use fixed version instead
+# mermaid_version = '10.6.1'
+
+
+def generate_api_docs(language):
+    subprocess.run(
+        [
+            'sphinx-apidoc',
+            os.path.join(os.path.dirname(__file__), '..', 'idf_ci'),
+            '--remove-old',
+            '-f',
+            '-H',
+            'API Reference',
+            '--no-headings',
+            '-t',
+            '_apidoc_templates',
+            '-o',
+            os.path.join(os.path.dirname(__file__), language, 'references', 'api'),
+        ]
+    )
---
+++ docs/en/index.rst
@@ -0,0 +1,11 @@
+################################
+ idf-ci |version| Documentation
+################################
+
+This documentation is for idf-ci. idf-ci is a python library for CI/CD of ESP-IDF projects
+
+.. toctree::
+   :maxdepth: 1
+   :caption: References
+
+   references/api/modules.rst
