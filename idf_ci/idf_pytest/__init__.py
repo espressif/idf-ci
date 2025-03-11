5e7b0a33435fb061b9c9e6d39d98e974a9535a70
Fu Hanxi <fuhanxi@espressif.com>
2025-03-11 15:31:53 +0100

    feat: support `idf-ci test collect`

--- idf_ci/idf_pytest/__init__.py
+++ idf_ci/idf_pytest/__init__.py
@@ -5,9 +5,12 @@
 __all__ = [
     'IDF_CI_PLUGIN_KEY',
     'IDF_CI_PYTEST_CASE_KEY',
+    'GroupedPytestCases',
     'IdfPytestPlugin',
+    'PytestCase',
     'get_pytest_cases',
 ]
 
+from idf_ci.idf_pytest.models import GroupedPytestCases, PytestCase
 from idf_ci.idf_pytest.plugin import IDF_CI_PLUGIN_KEY, IDF_CI_PYTEST_CASE_KEY, IdfPytestPlugin
 from idf_ci.idf_pytest.scripts import get_pytest_cases
