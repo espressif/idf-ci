3eb0f7de48888f34250b3bb805661a961a897250
igor.udot <igor.udot@espressif.com>
2026-06-04 16:04:53 +0800

    Revert "ci: retry failed jobs only"
    This reverts commit ebd437f2bcb2a704ffe5e998638f3c91d455c75b.

--- idf_ci/idf_gitlab/__init__.py
+++ idf_ci/idf_gitlab/__init__.py
@@ -6,12 +6,10 @@ __all__ = [
     'ArtifactParams',
     'build_child_pipeline',
     'pipeline_variables',
-    'prepare_retry_app_filter',
     'test_child_pipeline',
 ]
 
 
 from .api import ArtifactManager, ArtifactParams
 from .pipeline import build_child_pipeline, test_child_pipeline
-from .retry import prepare_retry_app_filter
 from .scripts import pipeline_variables
